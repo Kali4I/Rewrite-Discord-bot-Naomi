bcc666de6855eca936251eb07f122239a2b8c37e
AkiraSumato_01 <minewindowsteam@yandex.ru>
2018-09-22 01:29:16 +0400

    Fix

--- cogs/member.py
+++ cogs/member.py
@@ -51,7 +51,7 @@ class Member(object):
         """
         async with ctx.typing():
             try:
-                async with session.get(f"https://api.jikan.moe/search/manga/{query}") as response:
+                async with bot.session.get(f"https://api.jikan.moe/search/manga/{query}") as response:
                     data = await response.json()
                     embed = discord.Embed(color=random.randint(0x000000, 0xFFFFFF), timestamp=ctx.message.created_at, title=data["result"][0].get("title"))
                     embed.add_field(name="Описание:", value=f"{data['result'][0].get('description')}**[Read more about {data['result'][0].get('title')}...]({data['result'][0].get('url')})**", inline=True)
@@ -81,7 +81,7 @@ class Member(object):
         """
         async with ctx.typing():
             try:
-                async with session.get(f"https://api.jikan.moe/search/anime/{query}") as response:
+                async with bot.session.get(f"https://api.jikan.moe/search/anime/{query}") as response:
                     data = await response.json()
                     embed = discord.Embed(color=random.randint(0x000000, 0xFFFFFF), timestamp=ctx.message.created_at, title=data["result"][0].get("title"))
                     embed.add_field(name="Описание:", value=f"{data['result'][0].get('description')}**[Read more about {data['result'][0].get('title')}...]({data['result'][0].get('url')})**", inline=True)
