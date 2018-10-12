b4b272705a5c0f31164fffb03299deee3924f17f
AkiraSumato_01 <minewindowsteam@yandex.ru>
2018-10-12 18:27:07 +0700

    Fix

--- utils/NekosWrapper.py
+++ utils/NekosWrapper.py
@@ -1,21 +1,23 @@
 # python3.6
 # coding: utf-8
 
-## Nekos.life images wrapper for discord bot Naomi
-## https://github.com/AkiraSumato-01/Rewrite-Discord-Bot-Naomi
+# Nekos.life images wrapper for discord bot Naomi
+# https://github.com/AkiraSumato-01/Rewrite-Discord-Bot-Naomi
 
 import requests
 from random import choice
+
 
 class NekosWrapperError(Exception):
     """Base class for NekosWrapper exceptions."""
     pass
 
+
 class NekoNotInTags(NekosWrapperError):
     pass
 
 
-tags = [
+nekos_tags = [
     'feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
     'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk',
     'ngif', 'meow', 'tickle', 'lewd', 'feed', 'gecg', 'eroyuri', 'eron',
@@ -26,14 +28,15 @@ tags = [
     'neko', 'spank', 'cuddle', 'erok', 'fox_girl', 'boobs', 'Random_hentai_gif',
     'smallboobs', 'hug', 'ero']
 
-def get_neko(tag = None):
-    """Get image from nekos.life and return image URL"""
-    
-    if not tag:
-        tag = choice(tags)
 
-    if tag not in tags:
-        raise NekoNotInTags('Given tag not in possible tags list.')
+def get_neko(tag=None):
+    """Get image from nekos.life and return image URL"""
+
+    if not tag:
+        tag = choice(nekos_tags)
+
+    if tag not in nekos_tags:
+        raise NekoNotInTags('Given tag not in possible "nekos_tags" list.')
 
     response = requests.get('https://nekos.life/api/v2/img/' + tag)
     return response.json()['url']
