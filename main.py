f6c527d3943a5656218125ffa9de612c9e790fa4
AkiraSumato_01 <minewindowsteam@yandex.ru>
2018-10-21 23:51:36 +0700

    Fix

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # python3.6
-# -*- coding: utf-8 -*-
+# coding: utf-8
+
+# Разработка AkiraSumato_01
+# Специально для Discord сервера "Создание мода | Бесконечное Лето"
+# https://discord.gg/EDh8F3F
 
 import os
 import sys
@@ -11,15 +15,10 @@ import aiohttp
 import discord
 from discord.ext import commands
 
-prefix = os.getenv("PREFIX")
+prefix = os.getenv('PREFIX')
+activity = os.getenv('ACTIVITY')
 
 bot = commands.Bot(command_prefix=prefix)
-
-game_activity = os.getenv("ACTIVITY")
-
-async def start_session():
-    bot.session = aiohttp.ClientSession()
-
 bot.remove_command('help')
 
 extensions = ['cogs.member.fun',
@@ -31,50 +30,34 @@ extensions = ['cogs.member.fun',
               'cogs.admin.management',
               'cogs.owner']
 
+messages = [f'{len(bot.guilds)} серверов!',
+            f'{len(bot.users)} участников!',
+            f'{len(bot.emojis)} эмодзи!',
+            f'{len([x.name for x in bot.commands if not x.hidden])} команд!',
+            f'{prefix}help']
+
 if __name__ == '__main__':
     for extension in extensions:
         try:
             bot.load_extension(extension)
         except Exception as e:
-            print(f'[{time.ctime()}] Не удалось загрузить модуль {extension}.', file=sys.stderr)
+            print(f'Не удалось загрузить модуль {extension}.', file=sys.stderr)
             traceback.print_exc()
-
 
 @bot.event
 async def on_ready():
     print(f'[{time.ctime()}] Подключение успешно осуществлено!\nВ сети: {bot.user}')
-
-    await start_session()
-
+    
     async def presence():
-        sleeping = 10
-        messages = [f'{len(bot.guilds)} серверов!',
-                    f'{len(bot.users)} участников!',
-                    f'{len(bot.emojis)} эмодзи!',
-                    f'{len([x.name for x in bot.commands if not x.hidden])} команд!',
-                    f'{prefix}help']
         while not bot.is_closed():
             for msg in messages:
-                if self.game_activity == 'streaming':
-                    await self.change_presence(activity=discord.Streaming(name=msg, url='https://www.twitch.tv/%none%'))
+                if game_activity == 'streaming':
+                    await bot.change_presence(activity=discord.Streaming(name=msg, url='https://www.twitch.tv/%none%'))
                     await asyncio.sleep(10)
-                if self.game_activity == 'playing':
-                    await self.change_presence(activity=discord.Game(name=msg))
+                if game_activity == 'playing':
+                    await bot.change_presence(activity=discord.Game(name=msg))
                     await asyncio.sleep(10)
-    
-    def run(self):
-        self.remove_command('help')
-        for extension in self.extensions:
-            try:
-                self.load_extension(extension)
-            except Exception as e:
-                print(f'[{time.ctime()}] Не удалось загрузить модуль {extension}.', file=sys.stderr)
-                traceback.print_exc()
-        super().run(os.getenv('TOKEN'), reconnect=True)
-        
-    async def on_ready(self):
-        print(f'[{time.ctime()}] Подключение успешно осуществлено!\nВ сети: {self.user}')
-        self.loop.create_task(presence())
 
-if __name__ == '__main__':
-    Naomi(**{"BOTPREFIX": os.getenv('PREFIX'), "CINS": True, "FOM": False}).run()
+        bot.loop.create_task(self.presence())
+
+bot.run(os.getenv('TOKEN'), bot=True, reconnect=True)
