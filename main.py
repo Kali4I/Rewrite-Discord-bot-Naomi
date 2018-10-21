0ffdaa357ab677e034a7f1ca690c3938640401de
AkiraSumato-01 <40606774+AkiraSumato-01@users.noreply.github.com>
2018-10-21 23:13:54 +0700

    ...

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# python3.6
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import time
@@ -8,53 +11,57 @@ import aiohttp
 import discord
 from discord.ext import commands
 
-class Naomi(commands.AutoShardedBot):
-    def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or(kwargs.pop("BOTPREFIX")), case_insensitive=kwargs.pop("CINS"), fetch_offline_members=kwargs.pop("FOM"))
-        
-        self.session = aiohttp.ClientSession(loop=self.loop)
-        self.game_activity = 'playing'
-        self.extensions = ['cogs.member.fun',
-                           'cogs.member.info',
-                           'cogs.member.music',
-                           'cogs.member.utils',
-                           'cogs.system.error_handler',
-                           'cogs.system.logger',
-                           'cogs.admin',
-                           'cogs.owner']
+prefix = os.getenv("PREFIX")
 
-        self.messages = [f'{len(self.guilds)} серверов!',
-                         f'{len(self.users)} участников!',
-                         f'{len(self.emojis)} эмодзи!',
-                         f'{len([x.name for x in self.commands if not x.hidden])} команд!',
-                         f'{kwargs.pop("BOTPREFIX")}help']
-                         
-    def __repr__(self):
-        return "Я - Бот Наоми :)"
+bot = commands.Bot(command_prefix=prefix)
 
-    async def presence(self):
-        while not self.is_closed():
-            for msg in messages:
-                if self.game_activity == 'streaming':
-                    await self.change_presence(activity=discord.Streaming(name=msg, url='https://www.twitch.tv/%none%'))
-                    await asyncio.sleep(10)
-                if self.game_activity == 'playing':
-                    await self.change_presence(activity=discord.Game(name=msg))
-                    await asyncio.sleep(10)
-    
-    def run(self):
-        self.remove_command('help')
-        for extension in self.extensions:
-            try:
-                self.load_extension(extension)
-            except Exception as e:
-                print(f'[{time.ctime()}] Не удалось загрузить модуль {extension}.', file=sys.stderr)
-                traceback.print_exc()
-        super().run(os.getenv('TOKEN'), reconnect=True)
-        
-    async def on_ready(self):
-        print(f'[{time.ctime()}] Подключение успешно осуществлено!\nВ сети: {self.user}')
-        self.loop.create_task(presence())
+game_activity = os.getenv("ACTIVITY")
+
+async def start_session():
+    bot.session = aiohttp.ClientSession()
+
+bot.remove_command('help')
+
+extensions = ['cogs.member.fun',
+              'cogs.member.info',
+              'cogs.member.music',
+              'cogs.member.utils',
+              'cogs.system.error_handler',
+              'cogs.system.logger',
+              'cogs.admin.management',
+              'cogs.owner']
 
 if __name__ == '__main__':
-    Naomi(**{"BOTPREFIX": os.getenv('PREFIX'), "CINS": True, "FOM": False}).run()
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'[{time.ctime()}] Не удалось загрузить модуль {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+
+@bot.event
+async def on_ready():
+    print(f'[{time.ctime()}] Подключение успешно осуществлено!\nВ сети: {bot.user}')
+
+    await start_session()
+
+    async def presence():
+        sleeping = 10
+        messages = [f'{len(bot.guilds)} серверов!',
+                    f'{len(bot.users)} участников!',
+                    f'{len(bot.emojis)} эмодзи!',
+                    f'{len([x.name for x in bot.commands if not x.hidden])} команд!',
+                    f'{prefix}help']
+        while not bot.is_closed():
+            for msg in messages:
+                if game_activity == 'streaming':
+                    await bot.change_presence(activity=discord.Streaming(name=msg,
+                                                        url='https://www.twitch.tv/%none%'))
+                    await asyncio.sleep(sleeping)
+                if game_activity == 'playing':
+                    await bot.change_presence(activity=discord.Game(name=msg))
+                    await asyncio.sleep(sleeping)
+    bot.loop.create_task(presence())
+
+bot.run(os.getenv('TOKEN'), bot=True, reconnect=True)
