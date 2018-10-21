d5b3f19dca7dca19cfc8849698582a0130f4a4f4
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-10-21 18:34:45 +0300

    Make your code better
    yes

--- main.py
+++ main.py
@@ -10,55 +10,53 @@ from discord.ext import commands
 
 prefix = os.getenv("PREFIX")
 
-bot = commands.Bot(command_prefix=prefix)
+class Naomi(commands.AutoShardedBot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix=commands.when_mentioned_or(kwargs.pop("PREFIX")), case_insensitive=kwargs.pop("CINS"), fetch_offline_members=kwargs.pop("FOM"))
+        
+        self.session = aiohttp.ClientSession(loop=self.loop)
+        self.game_activity = 'playing'
+        self.extensions = ['cogs.member.fun',
+                           'cogs.member.info',
+                           'cogs.member.music',
+                           'cogs.member.utils',
+                           'cogs.system.error_handler',
+                           'cogs.system.logger',
+                           'cogs.admin',
+                           'cogs.owner']
 
-game_activity = 'playing'
+        self.messages = [f'{len(self.guilds)} серверов!',
+                         f'{len(self.users)} участников!',
+                         f'{len(self.emojis)} эмодзи!',
+                         f'{len([x.name for x in self.commands if not x.hidden])} команд!',
+                         f'{kwargs.pop("PREFIX")}help']
+                         
+    def __repr__(self):
+        return "Я - Бот Наоми :)"
 
-async def start_session():
-    bot.session = aiohttp.ClientSession()
-
-bot.remove_command('help')
-
-extensions = ['cogs.member.fun',
-              'cogs.member.info',
-              'cogs.member.music',
-              'cogs.member.utils',
-              'cogs.system.error_handler',
-              'cogs.system.logger',
-              'cogs.admin',
-              'cogs.owner']
-
-if __name__ == '__main__':
-    for extension in extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print(f'[{time.ctime()}] Не удалось загрузить модуль {extension}.', file=sys.stderr)
-            traceback.print_exc()
-
-
-@bot.event
-async def on_ready():
-    print(f'[{time.ctime()}] Подключение успешно осуществлено!\nВ сети: {bot.user}')
-
-    await start_session()
-
-    async def presence():
-        sleeping = 10
-        messages = [f'{len(bot.guilds)} серверов!',
-                    f'{len(bot.users)} участников!',
-                    f'{len(bot.emojis)} эмодзи!',
-                    f'{len([x.name for x in bot.commands if not x.hidden])} команд!',
-                    f'{prefix}help']
-        while not bot.is_closed():
+    async def presence(self):
+        while not self.is_closed():
             for msg in messages:
                 if game_activity == 'streaming':
-                    await bot.change_presence(activity=discord.Streaming(name=msg,
-                                                        url='https://www.twitch.tv/%none%'))
-                    await asyncio.sleep(sleeping)
+                    await self.change_presence(activity=discord.Streaming(name=msg, url='https://www.twitch.tv/%none%'))
+                    await asyncio.sleep(10)
                 if game_activity == 'playing':
-                    await bot.change_presence(activity=discord.Game(name=msg))
-                    await asyncio.sleep(sleeping)
-    bot.loop.create_task(presence())
+                    await self.change_presence(activity=discord.Game(name=msg))
+                    await asyncio.sleep(10)
+    
+    def run(self):
+        self.remove_command('help')
+        for extension in self.extensions:
+            try:
+                self.load_extension(extension)
+            except Exception as e:
+                print(f'[{time.ctime()}] Не удалось загрузить модуль {extension}.', file=sys.stderr)
+                traceback.print_exc()
+        self.run(os.getenv('TOKEN'), reconnect=True)
+        
+    async def on_ready(self):
+        print(f'[{time.ctime()}] Подключение успешно осуществлено!\nВ сети: {self.user}')
+        self.loop.create_task(presence())
 
-bot.run(os.getenv('TOKEN'), bot=True, reconnect=True)
+if __name__ == '__main__':
+    Naomi().run(**{"PREFIX": os.getenv("PREFIX"), "CINS": True, "FOM": False})
